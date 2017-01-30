f629acec5b7502fb6ba7be0667d1664253e5b820
Andrew Alles <andrew@lark-it.com>
2017-01-30 15:13:36 -0700

    make certificate and date validity optional

--- manifests/metadata.pp
+++ manifests/metadata.pp
@@ -2,7 +2,7 @@
 # it will need to be modified to permit multiple metadata providers
 define shibboleth::metadata(
   $provider_uri,
-  $cert_uri,
+  $cert_uri                 = undef,
   $backing_file_dir         = $::shibboleth::conf_dir,
   $backing_file_name        = inline_template("<%= @provider_uri.split('/').last  %>"),
   $cert_dir                 = $::shibboleth::conf_dir,
@@ -15,12 +15,29 @@ define shibboleth::metadata(
   $backing_file = "${backing_file_dir}/${backing_file_name}"
   $cert_file    = "${cert_dir}/${cert_file_name}"
 
-  # Get the Metadata signing certificate
-  exec{"get_${name}_metadata_cert":
-    path    => ['/usr/bin'],
-    command => "wget ${cert_uri} -O ${cert_file}",
-    creates => $cert_file,
-    notify  => Service['httpd','shibd'],
+  if $cert_uri {
+    # Get the Metadata signing certificate
+    exec{"get_${name}_metadata_cert":
+      path    => ['/usr/bin'],
+      command => "wget ${cert_uri} -O ${cert_file}",
+      creates => $cert_file,
+      notify  => Service['httpd','shibd'],
+    }
+    $aug_signature = [
+      'set MetadataProvider/MetadataFilter[2]/#attribute/type Signature',
+      "set MetadataProvider/MetadataFilter[2]/#attribute/certificate ${cert_file}",
+    ]
+  } else {
+    $aug_signature = 'rm MetadataProvider/MetadataFilter[2]/#attribute/type Signature'
+  }
+
+  if $metadata_filter_max_validity_interval > 0 {
+    $aug_valid_until = [
+      'set MetadataProvider/MetadataFilter[1]/#attribute/type RequireValidUntil',
+      "set MetadataProvider/MetadataFilter[1]/#attribute/maxValidityInterval ${metadata_filter_max_validity_interval}",
+    ]
+  } else {
+    $aug_valid_until = 'rm MetadataProvider/MetadataFilter[1]/#attribute/type RequireValidUntil'
   }
 
   # This puts the MetadataProvider entry in the 'right' place
@@ -46,10 +63,8 @@ define shibboleth::metadata(
       "set MetadataProvider/#attribute/uri ${provider_uri}",
       "set MetadataProvider/#attribute/backingFilePath ${backing_file}",
       "set MetadataProvider/#attribute/reloadInterval ${provider_reload_interval}",
-      'set MetadataProvider/MetadataFilter[1]/#attribute/type RequireValidUntil',
-      "set MetadataProvider/MetadataFilter[1]/#attribute/maxValidityInterval ${metadata_filter_max_validity_interval}",
-      'set MetadataProvider/MetadataFilter[2]/#attribute/type Signature',
-      "set MetadataProvider/MetadataFilter[2]/#attribute/certificate ${cert_file}",
+      $aug_valid_until,
+
     ],
     notify  => Service['httpd','shibd'],
     require => [Exec["get_${name}_metadata_cert"],Augeas["shib_${name}_create_metadata_provider"]],
